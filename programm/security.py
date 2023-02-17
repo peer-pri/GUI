271849744dc41fe942270df550c24eaec9684b6d
peer-pri <109176503+peer-pri@users.noreply.github.com>
2023-02-17 14:15:40 +0100

    Update security.py

--- programm/security.py
+++ programm/security.py
@@ -1,76 +1,138 @@
+import os
+from time import sleep 
 import tkinter as tk
-
-from cryptography.fernet import Fernet
-
-key1 = b'u8RAvUKIPE3w3VLklEqXv4466uCeEvlKxCvdvEjxDUs='
-
-
-def encrypter():
-    f = Fernet(key1)
-    message = word.get()
-    x = message.encode()
-    encryption = f.encrypt(x)
-    encrypted.delete(0, tk.END)  # clear the entry field
-    encrypted.insert(0, encryption)
+from tkinter import ttk
+from tkinter import * 
+from tkinter import filedialog
 
 
-def decrypter():
-    f = Fernet(key1)
-    message2 = encrypt.get()
-    decryption = f.decrypt(message2.encode())
-    decrypt.delete(0, tk.END)  # clear the entry field
-    decrypt.insert(0, decryption)
+def key_delete():
+    root = Tk()
+
+    # This is the section of code which creates the main window
+    root.geometry('389x114')
+    root.configure(background='#F0F8FF')
+    root.title('key devalued')
 
 
-def keycreator():
-    global key1
-    key1 = Fernet.generate_key()
-    key.insert(0, key1)
+    # This is the section of code which creates the a label
+    Label(root, text='The key for your file is devalued.', bg='#F0F8FF', font=('arial', 12, 'normal')).place(x=76, y=27)
 
 
-window = tk.Tk()
-window.title("Encrypter/Decrypter")
+    # This is the section of code which creates the a label
+    Label(root, text='You can delete the key now!', bg='#F0F8FF', font=('arial', 12, 'normal')).place(x=98, y=63)
 
-frame = tk.Frame(master=window, width=500, height=500)
-frame.pack()
 
-label1 = tk.Label(master=frame, text='Word')
-label1.place(x=10, y=5)
+    root.mainloop()
 
-word = tk.Entry(master=frame)
-word.place(x=0, y=25)
+def encrypt_done():
+    root = Tk()
 
-btn_convert_encrypt = tk.Button(master=frame, text="\N{DOWNWARDS BLACK ARROW}", command=encrypter)
-btn_convert_encrypt.place(x=40, y=50)
+    # This is the section of code which creates the main window
+    root.configure(background='#F0F8FF')
+    root.title('encrypted')
 
-label2 = tk.Label(master=frame, text='Encrypted')
-label2.place(x=10, y=80)
 
-encrypted = tk.Entry(master=frame)
-encrypted.place(x=0, y=100)
+    # This is the section of code which creates the a label
+    Label(root, text='The selected file is encrypted!', bg='#F0F8FF', font=('arial', 12, 'normal')).pack()
 
-label3 = tk.Label(master=frame, text='Encrypted')
-label3.place(x=250, y=5)
 
-encrypt = tk.Entry(master=frame)
-encrypt.place(x=250, y=25)
+    # This is the section of code which creates the a label
+    Label(root, text='The key to decrypt the file is there, where the encrypted file is.', bg='#F0F8FF', font=('arial', 12, 'normal')).pack()
 
-btn_convert_decrypt = tk.Button(master=frame, text="\N{DOWNWARDS BLACK ARROW}", command=decrypter)
-btn_convert_decrypt.place(x=290, y=50)
 
-label4 = tk.Label(master=frame, text='Word')
-label4.place(x=250, y=80)
+    root.mainloop()
 
-decrypt = tk.Entry(master=frame)
-decrypt.place(x=250, y=100)
+def encrypt(): # Your file will be encrypted
+    file = filedialog.askopenfilename(title='Select a file')
+    have_to_encrypt = open(file, "rb").read()
+    size = len(have_to_encrypt)
 
-label5 = tk.Label(master=frame, text='Key')
-label5.place(x=170, y=130)
+    progessBar['value'] = 20
+    root.update_idletasks()
 
-key = tk.Entry(master=frame)
-key.place(x=125, y=150)
+    key = os.urandom(size)
 
-btn_randmize = tk.Button(master=frame, text="Randmize", command=keycreator)
-btn_randmize.place(x=150, y=175)
+    progessBar['value'] = 40
+    root.update_idletasks()
 
-window.mainloop()
+    with open(f'{file}.key', "wb") as key_out:
+        key_out.write(key)
+    progessBar['value'] = 60
+    root.update_idletasks()
+
+    encrypted = bytes(a ^ b for (a, b) in zip(have_to_encrypt, key))
+
+    progessBar['value'] = 80
+    root.update_idletasks()
+
+    with open(file, "wb") as encrypted_out:
+        encrypted_out.write(encrypted)
+
+    progessBar['value'] = 100
+    root.update_idletasks()
+
+    encrypt_done()
+
+
+def decrypt(): # Your file will be decryptet
+    filename = filedialog.askopenfilename(title='Select the encrypted file')
+    key = filedialog.askopenfilename(title='Select the key for the encrypeted file')
+    file = open(filename, "rb").read()
+
+    progessBar['value'] = 30
+    root.update_idletasks()
+
+    key = open(key, "rb").read()
+    decrypted = bytes(a ^ b for (a, b) in zip(file, key))
+
+    progessBar['value'] = 60
+    root.update_idletasks()
+
+    with open(filename, "wb") as decrypted_out:
+        decrypted_out.write(decrypted)
+    
+    progessBar['value'] = 100
+    root.update_idletasks()
+
+    # call the done/key delete window
+    key_delete()
+    
+
+
+
+# This is a function which increases the progress bar value by the given increment amount
+def makeProgress():
+	progessBar['value']=progessBar['value'] + 1
+	root.update_idletasks()
+
+
+
+root = Tk()
+
+# This is the section of code which creates the main window
+root.geometry('297x119')
+root.configure(background='#F0F8FF')
+root.title('SafeFiler')
+
+
+# This is the section of code which creates a button
+Button(root, text='Encrypt', bg='#F0F8FF', font=('arial', 12, 'normal'), command=encrypt).place(x=22, y=26)
+
+
+# This is the section of code which creates a button
+Button(root, text='Decrypt', bg='#F0F8FF', font=('arial', 12, 'normal'), command=decrypt).place(x=121, y=26)
+
+
+# This is the section of code which creates a color style to be used with the progress bar
+progessBar_style = ttk.Style()
+progessBar_style.theme_use('clam')
+progessBar_style.configure('progessBar.Horizontal.TProgressbar', foreground='#F0F8FF', background='#F0F8FF')
+
+
+# This is the section of code which creates a progress bar
+progessBar=ttk.Progressbar(root, style='progessBar.Horizontal.TProgressbar', orient='horizontal', length=252, mode='determinate', maximum=100, value=1)
+progessBar.place(x=21, y=82)
+
+
+root.mainloop()
